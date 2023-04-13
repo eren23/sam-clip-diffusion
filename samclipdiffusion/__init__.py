665f5594e7e9e60de9dc102490d90ec526f43fa6
eren23 <erenak1996@gmail.com>
2023-04-13 22:02:58 +0300

    basic update

--- samclipdiffusion/__init__.py
+++ samclipdiffusion/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.0.1b"
+__version__ = "0.0.2b"
 from .image_segmenter import ImageSegmenter
--- samclipdiffusion/image_segmenter.py
+++ samclipdiffusion/image_segmenter.py
@@ -4,11 +4,17 @@ from PIL import Image, ImageDraw
 import clip
 import torch
 import numpy as np
+
+
 class ImageSegmenter:
     def __init__(self, sam_checkpoint="sam_vit_h_4b8939.pth", clip_model="ViT-B/32"):
-        self.mask_generator = SamAutomaticMaskGenerator(build_sam(checkpoint=sam_checkpoint))
+        self.mask_generator = SamAutomaticMaskGenerator(
+            build_sam(checkpoint=sam_checkpoint)
+        )
         self.clip_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.clip_model, self.clip_preprocess = clip.load(clip_model, device=self.clip_device)
+        self.clip_model, self.clip_preprocess = clip.load(
+            clip_model, device=self.clip_device
+        )
 
     @staticmethod
     def convert_box_xywh_to_xyxy(box):
@@ -27,7 +33,7 @@ class ImageSegmenter:
         black_image = Image.new("RGB", image.size, (0, 0, 0))
         transparency_mask = np.zeros_like(segmentation_mask, dtype=np.uint8)
         transparency_mask[segmentation_mask] = 255
-        transparency_mask_image = Image.fromarray(transparency_mask, mode='L')
+        transparency_mask_image = Image.fromarray(transparency_mask, mode="L")
         black_image.paste(segmented_image, mask=transparency_mask_image)
         return black_image
 
@@ -42,7 +48,9 @@ class ImageSegmenter:
         cropped_boxes = []
         for mask in masks:
             cropped_boxes.append(
-                self.segment_image(image, mask["segmentation"]).crop(self.convert_box_xywh_to_xyxy(mask["bbox"]))
+                self.segment_image(image, mask["segmentation"]).crop(
+                    self.convert_box_xywh_to_xyxy(mask["bbox"])
+                )
             )
 
         # Load CLIP
@@ -50,14 +58,16 @@ class ImageSegmenter:
 
         @torch.no_grad()
         def retriev(elements: list[Image.Image], search_text: str) -> int:
-            preprocessed_images = [preprocess(image).to(self.clip_device) for image in elements]
+            preprocessed_images = [
+                preprocess(image).to(self.clip_device) for image in elements
+            ]
             tokenized_text = clip.tokenize([search_text]).to(self.clip_device)
             stacked_images = torch.stack(preprocessed_images)
             image_features = model.encode_image(stacked_images)
             text_features = model.encode_text(tokenized_text)
             image_features /= image_features.norm(dim=-1, keepdim=True)
             text_features /= text_features.norm(dim=-1, keepdim=True)
-            probs = 100. * image_features @ text_features.T
+            probs = 100.0 * image_features @ text_features.T
             return probs[:, 0].softmax(dim=0)
 
         # Retrieve relevant images
@@ -67,21 +77,31 @@ class ImageSegmenter:
         # Generate overlay image
         segmentation_masks = []
         for seg_idx in indices:
-            segmentation_mask_image = Image.fromarray(masks[seg_idx]["segmentation"].astype('uint8') * 255)
+            segmentation_mask_image = Image.fromarray(
+                masks[seg_idx]["segmentation"].astype("uint8") * 255
+            )
             segmentation_masks.append(segmentation_mask_image)
 
-        overlay_image = Image.new('RGBA', image.size, (0, 0, 0, 0))
+        overlay_image = Image.new("RGBA", image.size, (0, 0, 0, 0))
         overlay_color = (255, 0, 0, 200)
 
         draw = ImageDraw.Draw(overlay_image)
         for segmentation_mask_image in segmentation_masks:
             draw.bitmap((0, 0), segmentation_mask_image, fill=overlay_color)
         # Overlay the masks on the original image
-        result_image = Image.alpha_composite(image.convert('RGBA'), overlay_image)
-        
+        result_image = Image.alpha_composite(image.convert("RGBA"), overlay_image)
+
         return result_image, segmentation_masks
 
-    def inpaint_image(self, image_path, search_text, prompt):
+    def inpaint_image(
+        self,
+        image_path,
+        search_text,
+        prompt,
+        num_inference_steps=50,
+        negative_prompt=None,
+        guide_image=7.5,
+    ):
         from PIL import ImageChops, ImageFilter
         from scipy import ndimage
         from diffusers import StableDiffusionInpaintPipeline
@@ -91,13 +111,9 @@ class ImageSegmenter:
         image = Image.open(image_path)
 
         # Call the segment_and_retrieve method to obtain the result image with segmentation masks
-        result_image, segmentation_masks = self.segment_and_retrieve(image_path, search_text)
-
-        # Extract the segmentation masks
-        # segmentation_masks = []
-        # for seg_idx in range(len(result_image.getbands())):
-        #     if result_image.getbands()[seg_idx] == 'A':
-        #         segmentation_masks.append(result_image.getchannel(seg_idx))
+        result_image, segmentation_masks = self.segment_and_retrieve(
+            image_path, search_text
+        )
 
         # Merge masks
         merged_mask = reduce(ImageChops.add, segmentation_masks)
@@ -110,10 +126,14 @@ class ImageSegmenter:
         structuring_element = ndimage.generate_binary_structure(2, 2)
 
         # Perform erosion to remove small isolated pixels
-        eroded_mask = ndimage.binary_erosion(mask_array, structure=structuring_element, iterations=2)
+        eroded_mask = ndimage.binary_erosion(
+            mask_array, structure=structuring_element, iterations=2
+        )
 
         # Perform dilation to fill small holes in the object masks
-        dilated_mask = ndimage.binary_dilation(eroded_mask, structure=structuring_element, iterations=2)
+        dilated_mask = ndimage.binary_dilation(
+            eroded_mask, structure=structuring_element, iterations=2
+        )
 
         # Convert the numpy array back to a PIL image
         filtered_mask = Image.fromarray(np.uint8(dilated_mask) * 255)
@@ -128,9 +148,15 @@ class ImageSegmenter:
 
         pipe = pipe.to("cuda")
 
-        image_inpainting = pipe(prompt=prompt, image=image_source_for_inpaint, mask_image=image_mask_for_inpaint).images[0]
+        image_inpainting = pipe(
+            prompt=prompt,
+            image=image_source_for_inpaint,
+            mask_image=image_mask_for_inpaint,
+            num_inference_steps=num_inference_steps,
+            negative_prompt=negative_prompt,
+            guidance_scale=guidance_scale,
+        ).images[0]
 
         image_inpainting = image_inpainting.resize((image.size[0], image.size[1]))
 
         return image_inpainting
-        
